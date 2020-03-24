58e073300967592be5e4a5731faa247e6f23baa4
Austin Gray <Austin.m.gray210@gmail.com>
2020-03-24 10:17:56 -0500

    init

---
+++ src/adventureGame.java
@@ -0,0 +1,37 @@
+public class adventureGame {
+    public static void main(String[] args) {
+
+
+
+
+// Scanner something = new Scanner(System.in);
+
+
+
+// TODO: Ask if the user is ready to start. If they type in "yes", start the game.
+
+// TODO: Ask the user for their name. Store this as a variable to personalize the adventure.
+
+// TODO: A classic RPG will have the hero going through different scenarios and battling their enemies.
+    // Display enemy stats and hero stats. Properties and values can be assigned by you.
+    // For example: Health, Attack Points, etc.
+
+// TODO: Decide on what actions your hero can take.
+    // Attack (decreases enemyHealth)
+    // Drink potion (adds to heroHealth)
+    // Run? (ends the game)
+
+// TODO: Keep asking for user input until the enemyHealth reaches 0, then end the game.
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
